7735ce3ed5f6ef37a53cf68cb8554dcd1d6acc14
xuyangshen123 <shen.xuy@northeastern.edu>
2025-04-04 09:13:57 -0400

    Added generator modules from sumit_dev branch

---
+++ Generator_Main.py
@@ -0,0 +1,11 @@
+from Generator_Trainer import train_dungeon_generator, generate_dungeon_with_model, visualize_dungeon, export_dungeon
+
+trained_agent, trained_envs = train_dungeon_generator(num_episodes=2000)
+generated_dungeon = generate_dungeon_with_model(trained_agent, 7)
+visualize_dungeon(generated_dungeon)
+
+difficulty = 8
+print(f"Difficulty: {difficulty}")
+generated_dungeon = generate_dungeon_with_model(trained_agent, difficulty)
+visualize_dungeon(generated_dungeon)
+gen_size, gen_start_pos, gen_end_pos, gen_grid = export_dungeon(generated_dungeon)
